0a0e44f8f1c1d396ed37838299c30a2b358671f6
quentindurpoix <durpoixquentin@gmail.com>
2017-01-18 15:49:26 +0100

    Commit Index + fichiers
    index terminé

--- roles/index.php
+++ roles/index.php
@@ -19,23 +19,51 @@
 
 $dbh = new PDO('mysql:host=localhost;dbname=phalcon-td0', 'root','');
 
-$stmt = $dbh->prepare('SELECT name, count(idrole), role.id FROM role JOIN user ON user.idrole = role.id GROUP BY role.id, name');
-$stmt->execute();
+
 ?>
-</br></br></br></br>
+
 <div class="container">
+    </br></br>
+    <legend>Roles</legend>
+    </br></br>
+    <form method="POST" action="./add">
+        <input type="submit" name="ok" value="Ajouter un rôle.." >
+    </form>
+    </br></br></br></br>
     <table class="table  table-bordered">
         <tr>
             <th>Name <a href="?col=1&tri=up"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></a>
                   <a href="?col=1&tri=down"><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></a>
             </th>
-            <th>Nb Users <a href="?col=1&tri=up"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></a>
-                  <a href="?col=1&tri=down"><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></a></th>
+            <th>Nb Users <a href="?col=2&tri=up"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></a>
+                  <a href="?col=2&tri=down"><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></a></th>
             <th>Actions</th>
          </tr>
 <?php
+
+if(!isset($_GET['col'])||!isset($_GET['tri'])){
+    $stmt = $dbh->prepare('SELECT name, count(idrole), role.id FROM role JOIN user ON user.idrole = role.id GROUP BY role.id, name ORDER by name');
+}else{
+    if($_GET['col']=='1'){
+        if($_GET['tri']=='up'){
+            $stmt = $dbh->prepare('SELECT name, count(idrole), role.id FROM role JOIN user ON user.idrole = role.id GROUP BY role.id, name ORDER by name');
+        }else{
+            $stmt = $dbh->prepare('SELECT name, count(idrole), role.id FROM role JOIN user ON user.idrole = role.id GROUP BY role.id, name ORDER by name desc');
+        }
+
+    }else{
+        if($_GET['tri']=='up'){
+            $stmt = $dbh->prepare('SELECT name, count(idrole), role.id FROM role JOIN user ON user.idrole = role.id GROUP BY role.id, name ORDER by count(idrole)');
+        }else{
+            $stmt = $dbh->prepare('SELECT name, count(idrole), role.id FROM role JOIN user ON user.idrole = role.id GROUP BY role.id, name ORDER by count(idrole) desc');
+        }
+    }
+
+}
+
+$stmt->execute();
     while ($result = $stmt->fetch(PDO::FETCH_ASSOC)){
-    echo '<tr><td> '.$result['name'].'<br></td><td> '.$result['count(idrole)'].'<br></td><td><span class="glyphicon glyphicon-cog"></span></td></tr>';
+    echo '<tr><td> '.$result['name'].'<br></td><td> '.$result['count(idrole)'].'<br></td><td><a href="./show?id='.$result['id'].'"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></a><a href="./update?id='.$result['id'].'"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span></a><a href="./delete?id='.$result['id'].'"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a></tr>';
     };
 
 ?>
---
+++ roles/show.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Coco
+ * Date: 18/01/2017
+ * Time: 15:46
+ */
